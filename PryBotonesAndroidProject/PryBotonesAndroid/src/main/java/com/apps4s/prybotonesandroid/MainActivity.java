d4904fa505a8f36384547a0438be03bff66ec6ff
Henry AT <yiperu@gmail.com>
2013-10-24 17:19:14 -0300

    Se configuro para que funcione el evento touch

--- PryBotonesAndroidProject/PryBotonesAndroid/src/main/java/com/apps4s/prybotonesandroid/MainActivity.java
+++ PryBotonesAndroidProject/PryBotonesAndroid/src/main/java/com/apps4s/prybotonesandroid/MainActivity.java
@@ -3,13 +3,43 @@ package com.apps4s.prybotonesandroid;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends Activity {
+
+    int contador;
+    TextView tv1;
+    Button  btn1, btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        contador = 0;
+        tv1 = (TextView)findViewById(R.id.id_tv1);
+        btn1 = (Button)findViewById(R.id.id_btn1);
+        btn2 = (Button)findViewById(R.id.id_btn2);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                contador++;
+                tv1.setText("El total es: "+contador);
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                contador--;
+                tv1.setText("El total es: "+contador);
+            }
+        });
+
     }
 
 
